ded05c059a29ceb5a21f82a91f29afadbce05684
metarmask <metarmask@users.noreply.github.com>
2016-07-20 14:27:28 +0200

    Handle update from old extension

--- background.js
+++ background.js
@@ -3,12 +3,20 @@
 chrome.runtime.onInstalled.addListener(details => {
 	if(details.reason === "install"){
 		localStorage.tabsOpened = 0;
+		localStorage.ready = "true";
+	}else if(details.reason === "update"){
+		chrome.storage.sync.get("tabsOpened", ({tabsOpened = 0}) => {
+			localStorage.tabsOpened = tabsOpened;
+			localStorage.ready = "true";
+		});
 	}
 });
 
 chrome.tabs.onCreated.addListener(tab => {
-	localStorage.tabsOpened = parseInt(localStorage.tabsOpened) + 1;
-	chrome.storage.sync.set({tabsOpened: parseInt(localStorage.tabsOpened)});
+	if(localStorage.ready){
+		localStorage.tabsOpened = parseInt(localStorage.tabsOpened) + 1;
+		chrome.storage.sync.set({tabsOpened: parseInt(localStorage.tabsOpened)});
+	}
 });
 
 chrome.storage.onChanged.addListener((changes, area) => {
